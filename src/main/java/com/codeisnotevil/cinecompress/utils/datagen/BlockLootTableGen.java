00fbe564fd72338fd192764c7fe610b1d7e2dcb0
CodeIsNotEvil <codeisnotevil@gmail.com>
2022-09-07 08:06:11 +0200

    changes datageneration for loot tables to auto add them for compressed blocks.

--- src/main/java/com/codeisnotevil/cinecompress/utils/datagen/BlockLootTableGen.java
+++ src/main/java/com/codeisnotevil/cinecompress/utils/datagen/BlockLootTableGen.java
@@ -1,6 +1,9 @@
 package com.codeisnotevil.cinecompress.utils.datagen;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.codeisnotevil.cinecompress.registry.CompressedBlocks;
 
@@ -16,67 +19,31 @@ public class BlockLootTableGen extends FabricBlockLootTableProvider{
 
     @Override
     protected void generateBlockLootTables() {
-
-        // Stones
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_COBBLED_DEEPSLATE_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_DEEPSLATE_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_COBBLESTONE_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_STONE_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_OBSIDIAN_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_DIORITE_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_ANDESITE_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_GRANITE_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_TUFF_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_CALCITE_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_DRIPSTONE_BLOCK_BLOCKS);
-
-        // Dirty
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_DIRT_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_SAND_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_RED_SAND_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_CLAY_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_GRAVEL_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_TERRACOTTA_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_MOSS_BLOCK_BLOCKS);
-
-        // Hellish
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_NETHERRACK_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_SOUL_SAND_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_NETHER_BRICKS_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_GLOWSTONE_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_MAGMA_BLOCK_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_BASALT_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_BLACKSTONE_BLOCKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_SOUL_SOIL_BLOCKS);
-
-        // Endish
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_END_STONE);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_END_STONE_BRICKS);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_PURPUR_BLOCK);
-
-        // Woods
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_OAK_LOG);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_STRIPPED_OAK_LOG);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_BIRCH_LOG);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_STRIPPED_BIRCH_LOG);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_SPRUCE_LOG);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_STRIPPED_SPRUCE_LOG);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_JUNGLE_LOG);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_STRIPPED_JUNGLE_LOG);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_ACACIA_LOG);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_STRIPPED_ACACIA_LOG);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_DARK_OAK_LOG);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_STRIPPED_DARK_OAK_LOG);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_CRIMSON_STEM);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_STRIPPED_CRIMSON_STEM);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_WARPED_STEM);
-        generateSelfDropLootTableForAll(CompressedBlocks.COMPRESSED_STRIPPED_WARPED_STEM);
-        
+        generateSelfDropForEveryCompressedBlock();
     }
 
     private void generateSelfDropLootTableForAll(ArrayList<Block> blocks) {
         for (Block block : blocks) {
             addDrop(block);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void generateSelfDropForEveryCompressedBlock() {
+        Pattern compressed = Pattern.compile("COMPRESSED_[A-Z_]*");
+        Field[] compressedBlocksFields = CompressedBlocks.class.getDeclaredFields();
+        for (Field field : compressedBlocksFields) {
+            Matcher fieldIsCompressedBlock = compressed.matcher(field.getName());
+            if (fieldIsCompressedBlock.matches()) {
+                field.setAccessible(true);
+                try {
+                    generateSelfDropLootTableForAll((ArrayList<Block>) field.get(null));
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
     
